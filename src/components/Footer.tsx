852b8c381c2030bdfe1f3e3d890c62f8de340c67
nfrd <vincent.nafrada@gmail.com>
2026-09-19 09:15:22 -0400

    Remove footer social links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,11 +2,6 @@ export default function Footer() {
   return (
     <div className="footer">
       <span className="footer-brand">PLURAL WORLD</span>
-      <div className="footer-links">
-        <a href="#">Instagram</a>
-        <a href="#">LinkedIn</a>
-        <a href="#">Dribbble</a>
-      </div>
     </div>
   )
 }
